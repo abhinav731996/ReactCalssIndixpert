6b187b4e9560355918c41acffbfcf87fbe9b1300
Abhinav <abhinav731996@gmail.com>
2026-04-04 09:45:32 +0530

    Add to cart

--- src/store/slices/CartSlice.jsx
+++ src/store/slices/CartSlice.jsx
@@ -14,7 +14,7 @@ export const cartSlice = createSlice({
         category: "xyz",
         price: 10,
         quentity: 0,
-        totalPirce: 0,
+        totalPrice: 0,
       },
       {
         id: 2,
@@ -24,10 +24,10 @@ export const cartSlice = createSlice({
         category: "xyz",
         price: 10,
         quentity: 0,
-        totalPirce: 0,
+        totalPrice: 0,
       },
     ],
-    totalQuantities: 0,
+    totalQuentities: 0,
     cartTotal: 0,
     shippingCost: 0,
     tax: 18, // its in percentage format
@@ -50,12 +50,27 @@ export const cartSlice = createSlice({
         category: payload.category,
         price: payload.price,
         quentity: payload.quentity,
-        totalPirce: payload.price * payload.quentity,
+        totalPrice: payload.price * payload.quentity,
       };
       state.cartItems = [...state.cartItems, newItem];
     },
+        removeFromCart: (state, action) => {
+        state.cartItems = state.cartItems.filter((product) => product.id !== action.payload)
+        
+    },
+    updateQuentity: (state, action) => {
+  const index = state.cartItems.findIndex(
+    (item) => item.id === action.payload.id
+  );
+
+  if (index !== -1) {   // ✅ IMPORTANT
+    state.cartItems[index].quentity = action.payload.quentity;
+    state.cartItems[index].totalPrice =
+      state.cartItems[index].price * action.payload.quentity;
+  }
+},
   },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, updateQuentity } = cartSlice.actions;
 export default cartSlice.reducer;
